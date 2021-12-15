f7c95a4676c9c33248619a15f1a03aa204284561
Emma Brown <emmakatherinebrown97@gmail.com>
2021-12-14 20:37:25 -0600

    Initial commit

---
+++ src/js/homepage.js
@@ -0,0 +1,12 @@
+function App() {
+    return (
+      <html>
+        <div className="App">
+          <h1>React Boilerplate Works!!! 🎆</h1>
+        </div>
+      </html>
+    );
+  }
+  
+export default App;
+  
